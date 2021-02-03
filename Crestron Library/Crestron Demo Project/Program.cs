8150d022153b6425bada3e1ebfaeffef31072087
Zekael <Nilssen-m@hotmail.com>
2021-02-03 06:12:25 +0100

    Added new Solution for Crestron Proof Of Concept

---
+++ Crestron Library/Crestron Demo Project/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Crestron_Demo_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
