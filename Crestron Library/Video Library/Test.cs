2df4147de154a862c6970ddb101d0fd00b7a18a9
MegaAnda123 <andrehelland99@gmail.com>
2021-02-15 15:36:26 +0100

    VideoDeviceInterface

--- Crestron Library/Video Library/Test.cs
+++ Crestron Library/Video Library/Test.cs
@@ -1,12 +1,170 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading;
+//using System.Threading.Tasks;
+//using LibVLCSharp.Shared;
+//using AForge.Video.DirectShow;
+using OpenCvSharp;
 
 namespace Video_Library {
 	class Test {
 		public Test() {
-			throw new NotImplementedException();
-			//var devices = await Windows.Devices.Enumeration.DeviceInformation.FindAllAsync(Windows.Devices.Enumeration.DeviceClass.VideoCapture);
-			//deviceList = new List<Windows.Devices.Enumeration.DeviceInformation>();
+
 		}
+
+		static void Main(string[] args) {
+			var videoDevice = new VideoDeviceInterface(1);
+
+			Mat frame = new Mat();
+			while (true) {
+				if(videoDevice.tryReadFrameBuffer(out frame)) {
+					Cv2.ImShow("test", frame);
+				}
+				Cv2.WaitKey(1);
+			}
+
+
+			//test.aForge();
+			//test.openCV();
+			//test.openCV2();
+			//test.openCV3();
+			//test.openCV4();
+		}
+
+		public void openCV4() {
+			//var capture = new VideoCapture(3);
+			var capture = new VideoCapture(1, VideoCaptureAPIs.DSHOW);
+
+			capture.Set(VideoCaptureProperties.FrameWidth, 1280);
+			capture.Set(VideoCaptureProperties.FrameHeight, 720);
+
+			var frame = new Mat();
+			while (true) {
+				capture.Read(frame);
+
+				Cv2.ImShow("ok", frame);
+				Cv2.WaitKey(1);
+
+			}
+
+		}
+
+		public void openCV3() {
+			var capture = new VideoCapture(1);
+
+			Console.WriteLine(capture.GetBackendName());
+
+			capture.Open(0);
+			using (var normalWindow = new Window("normal"))
+			using (var image = new Mat()) {
+				while (true) {
+					capture.Read(image);
+					normalWindow.ShowImage(image);
+				}
+			}
+		}
+
+		public void openCV2() {
+			var capture = new VideoCapture(0);
+			//capture.Open(0, VideoCaptureAPIs.ANY);
+			using (var normalWindow = new Window("normal"))
+			using (var image = new Mat()) {
+				while (true) {
+					/* start camera */
+					capture.Read(image);
+					if (!image.Empty()) {
+						break;
+					}
+					Console.WriteLine("no frame");
+				}
+			}
+			using (var normalWindow = new Window("normal"))
+			using (var image = new Mat()) {
+				double counter = 0;
+				double seconds = 0;
+				var watch = Stopwatch.StartNew();
+				while (true) {
+					capture.Read(image);
+					if (image.Empty()) {
+						break;
+					}
+
+					normalWindow.ShowImage(image);
+
+					counter++;
+					seconds = watch.ElapsedMilliseconds / (double)1000;
+					if (seconds >= 3) {
+						watch.Stop();
+						break;
+					}
+				}
+				var fps = counter / seconds;
+				Console.WriteLine(fps);
+			}
+		}
+
+		public void openCV() {
+			var capture = new VideoCapture();
+			//capture.Set(VideoCaptureProperties.FrameWidth, 640);
+			//capture.Set(VideoCaptureProperties.FrameHeight, 480);
+			capture.Open(2);
+			if (!capture.IsOpened())
+				throw new Exception("capture initialization failed");
+
+			var fs = FrameSource.CreateFrameSource_Camera(2);
+
+			using (var normalWindow = new Window("normal")) {
+				var normalFrame = new Mat();
+				while (true) {
+					Console.WriteLine("ok");
+					capture.Read(normalFrame);
+					if (normalFrame.Empty()) {
+						Console.WriteLine("ffs");
+						break;
+					}
+					//normalWindow.ShowImage(normalFrame);
+					//srWindow.ShowImage(srFrame);
+
+					Cv2.ImShow("ok", normalFrame);
+					Cv2.WaitKey(100);
+				}
+			}
+		}
+
+
+		//public void aForge() {
+		//	// enumerate video devices
+		//	var videoDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+		//	foreach (FilterInfo ffs in videoDevices) {
+		//		Console.WriteLine(ffs.Name);
+		//	}
+
+		//	VideoCaptureDevice captureDevice = new VideoCaptureDevice(videoDevices[0].MonikerString);
+
+		//	captureDevice.Start();
+		//	Thread.Sleep(5000);
+		//	captureDevice.Stop();
+
+		//}
+
+
+		/*
+		public static async void vlc() {
+
+			using var libvlc = new LibVLC();
+			using var mediaPlayer = new MediaPlayer(libvlc);
+			using var media = new Media(libvlc, "dshow://", FromType.FromLocation);
+
+			mediaPlayer.Play(media);
+
+			await Task.Delay(5000);
+
+			mediaPlayer.Stop();
+
+
+			throw new NotImplementedException();
+		}
+		*/
 	}
 }
---
+++ Crestron Library/Video Library/VideoDeviceInterface.cs
@@ -0,0 +1,74 @@
+﻿using OpenCvSharp;
+using System;
+using System.Collections.Concurrent;
+using System.Threading;
+
+namespace Video_Library {
+
+	/// <summary>
+	/// Class for interfacing with video capture devices (usb webcam, capture card, etc).
+	/// Class maintains a frame buffer where frames can be read from concurrently.
+	/// </summary>
+	/// <author>Andre Helland</author>
+	public class VideoDeviceInterface: IDisposable {
+		private readonly VideoCapture capture;
+		private readonly ConcurrentQueue<Mat> frameBuffer;
+		private bool captureFrames;
+
+		//DSHOW: Windows api for video devices.
+		public VideoDeviceInterface(int index = 0, VideoCaptureAPIs API = VideoCaptureAPIs.DSHOW) {
+			capture = new VideoCapture(index, API);
+			frameBuffer = new ConcurrentQueue<Mat>();
+			Thread thread = new Thread(FrameCaptureThread);
+			captureFrames = true;
+			thread.Start();
+
+
+			//TODO: throw exception for invalid index.
+			//TODO: implement device watchdog timer.
+
+
+
+			//var frame = new Mat();
+			//while (true) {
+
+			//	if(tryReadFrameBuffer(out frame)) {
+			//		Cv2.ImShow("ffs", frame);
+			//	}
+
+
+			//	Cv2.WaitKey(1);
+			//}
+		}
+
+		/// <summary>
+		/// Thread starting from class constructor to concurrently fill frame buffer with frames.
+		/// </summary>
+		private void FrameCaptureThread() {
+			var frame = new Mat();
+			while (captureFrames) {
+				capture.Read(frame);
+				frameBuffer.Enqueue(frame);
+			}
+			capture.Release();
+		}
+
+		/// <summary>
+		/// Method for accessing frames in frame buffer concurrently.
+		/// </summary>
+		/// <param name="framePointer">Pointer for method to fill with frame data</param>
+		/// <returns>If frame buffer contained new frames and was read successfully</returns>
+		public bool tryReadFrameBuffer(out Mat framePointer) {
+			return frameBuffer.TryDequeue(out framePointer);
+		}
+
+		/// <summary>
+		/// Stops capturing frames and release capture device resources.
+		/// </summary>
+		public void Dispose() {
+			captureFrames = false;
+			GC.SuppressFinalize(this);
+		}
+
+	}
+}
