eb4af32fc860108d4dac976c47f024707b4672f3
MegaAnda123 <andrehelland99@gmail.com>
2021-02-19 14:29:55 +0100

    Cursor position script

--- Distributed Instrument Cluster/Crestron Library/CommandParser.cs
+++ Distributed Instrument Cluster/Crestron Library/CommandParser.cs
@@ -6,5 +6,26 @@ using System.Threading.Tasks;
 
 namespace Crestron_Library {
 	class CommandParser {
+		Commands commands = new Commands();
+
+
+
+		public List<byte> parse(string idk) {
+			List<byte> bytes = new List<byte>();
+
+
+
+
+
+			return bytes;
+
+		}
+
+
+
+
+
+
+
 	}
 }
