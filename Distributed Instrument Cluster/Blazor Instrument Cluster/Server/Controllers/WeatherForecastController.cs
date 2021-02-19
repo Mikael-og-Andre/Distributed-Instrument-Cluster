20062627974964c782c768facadbf89cba6ae75a
Mikael Nilssen <Nilssen-m@hotmail.com>
2021-02-19 19:08:19 +0100

    Re Create blazor projects

---
+++ Distributed Instrument Cluster/Blazor Instrument Cluster/Server/Controllers/WeatherForecastController.cs
@@ -0,0 +1,35 @@
+﻿using Blazor_Instrument_Cluster.Shared;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blazor_Instrument_Cluster.Server.Controllers {
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger) {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get() {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
---
+++ Distributed Instrument Cluster/Blazor Instrument Cluster/Shared/WeatherForecast.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Blazor_Instrument_Cluster.Shared {
+    public class WeatherForecast {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public string Summary { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    }
+}
