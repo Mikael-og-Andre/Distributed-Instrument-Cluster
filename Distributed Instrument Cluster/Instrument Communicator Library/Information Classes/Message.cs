e82869a866bb52e1a8621abb5458ef87fa3c6eb0
Mikael Nilssen <Nilssen-m@hotmail.com>
2021-03-03 03:08:25 +0100

    Updated Serialization In message object

--- Distributed Instrument Cluster/Instrument Communicator Library/Information Classes/Message.cs
+++ Distributed Instrument Cluster/Instrument Communicator Library/Information Classes/Message.cs
@@ -1,67 +1,100 @@
-﻿
-
+﻿using Instrument_Communicator_Library.Interface;
 using System;
-using System.Text;
-using Instrument_Communicator_Library.Interface;
+using System.Collections.Generic;
 
 namespace Instrument_Communicator_Library {
+
 	/// <summary>
 	/// Represents a message to put in the queue concurrent queue when sending to the client
 	/// <author>Mikael Nilssen</author>
 	/// </summary>
 	public class Message : ISerializeableObject {
-        private protocolOption option;  //protocol option enum that tells server what protocol to use when sending
-        private string messageString;	//String to be sent to the server
+		private protocolOption option;  //protocol option enum that tells server what protocol to use when sending
+		private string messageString;   //String to be sent to the server
 
-        /// <summary>
-        /// Constructor
-        /// </summary>
-        /// <param name="option">protocol option for the message</param>
-        /// <param name="messageStringArray">array of strings that will be sent to the client</param>
-        public Message(protocolOption option, string messageString) {
-            this.option = option;
-            this.messageString = messageString;
-        }
+		/// <summary>
+		/// Constructor
+		/// </summary>
+		/// <param name="option">protocol option for the message</param>
+		/// <param name="messageString">array of strings that will be sent to the client</param>
+		public Message(protocolOption option, string messageString) {
+			this.option = option;
+			this.messageString = messageString;
+		}
 
-        /// <summary>
-        /// Returns array of strings, throws exception if not meant to be a multiMessage object
-        /// </summary>
-        /// <returns>String array with messages</returns>
-        public string getMessage() {
-            return this.messageString;
-        }
+		/// <summary>
+		/// Returns array of strings, throws exception if not meant to be a multiMessage object
+		/// </summary>
+		/// <returns>String array with messages</returns>
+		public string getMessage() {
+			return this.messageString;
+		}
 
-        /// <summary>
-        /// Get the protocol intended for the message
-        /// </summary>
-        /// <returns></returns>
-        public protocolOption getProtocol() {
-            return this.option;
-        }
+		/// <summary>
+		/// Get the protocol intended for the message
+		/// </summary>
+		/// <returns></returns>
+		public protocolOption getProtocol() {
+			return this.option;
+		}
 
-        public byte[] getBytes() {
-	        byte[] completeBytes = new byte[2048];
-	        byte[] optionBytes = Encoding.ASCII.GetBytes(this.option.ToString(), 0, 1024);
-	        byte[] messageBytes = Encoding.ASCII.GetBytes(this.messageString,0,1024);
-			
-			System.Buffer.BlockCopy(optionBytes,0,completeBytes,0,1024);
-			System.Buffer.BlockCopy(messageBytes,0,completeBytes,1024,1024);
+		public byte[] getBytes() {
+			//Byte array with the size of char for each element in
+			byte[] completeBytes = new byte[(option.ToString().Length * messageString.Length * sizeof(char)) + 1];
+			char[] optionChars = option.ToString().ToCharArray();
+			char[] messageChars = messageString.ToCharArray();
+
+			int i = 0;
+			//Add Option chars to byte array
+			for (i = 0; i < optionChars.Length; i++) {
+				completeBytes[i] = (byte)optionChars[i];
+			}
+			//Add max byte to show where first string ends
+			i++;
+			completeBytes[i] = byte.MaxValue;
+			//Add messageBytes to byte array after the null byte
+			for (int j = 0; j < messageChars.Length; i++, j++) {
+				completeBytes[i] = (byte)messageChars[j];
+			}
 
 			return completeBytes;
-        }
+		}
 
-        public object getObject(byte[] arrayBytes) {
-	        byte[] optionBytes = new byte[1024];
-	        byte[] messagebytes = new byte[1024];
-			System.Buffer.BlockCopy(arrayBytes,0,optionBytes,0,1024);
-			System.Buffer.BlockCopy(arrayBytes, 1024, messagebytes, 0, 1024);
+		/// <summary>
+		/// Convert array Of bytes created by the get btyes method back into a message object
+		/// </summary>
+		/// <param name="arrayBytes">Array of bytes generated from get bytes method from the same object type</param>
+		/// <returns>Object</returns>
+		public object getObject(byte[] arrayBytes) {
+			List<char> optionChars = new List<char>();
+			List<char> messageChars = new List<char>();
 
-			string optionString = Encoding.ASCII.GetString(optionBytes);
-			string messageString = Encoding.ASCII.GetString(messagebytes);
-
+			bool seenMaxByte = false;
+			for (int i = 0; i < arrayBytes.Length; i++) {
+				byte currentByte = arrayBytes[i];
+				//Check if max byte is there so you start writing to the message byte list
+				if (currentByte.Equals(Byte.MaxValue)) {
+					seenMaxByte = true;
+				}
+				//Write to either option byte or message byte list
+				if (seenMaxByte) {
+					messageChars.Add((char)currentByte);
+				} else {
+					optionChars.Add((char)currentByte);
+				}
+			}
+			//Convert to string and option
+			char[] optionCharArray = optionChars.ToArray();
+			char[] messageCharArray = messageChars.ToArray();
+			//Convert to string
+			string optionString = optionCharArray.ToString();
+			string messageString = messageCharArray.ToString();
+			//Recreate enum
 			protocolOption option = (protocolOption)Enum.Parse(typeof(protocolOption), optionString, true);
+
+
 			Message msg = new Message(option, messageString);
 			return msg;
-        }
-    }
+		}
+	}
 }
