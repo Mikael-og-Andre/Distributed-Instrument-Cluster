394994f8d662d6b42f3f7975a266f236f75d7e61
Mikael Nilssen <Nilssen-m@hotmail.com>
2021-03-14 11:42:23 +0100

    Unit test for networking Lib

--- Distributed Instrument Cluster/NetworkingLibrary/NetworkingOperations.cs
+++ Distributed Instrument Cluster/NetworkingLibrary/NetworkingOperations.cs
@@ -11,6 +11,74 @@ namespace Networking_Library {
 	public static class NetworkingOperations {
 
 		/// <summary>
+		/// Receive an string with the given socket
+		/// </summary>
+		/// <param name="connectionSocket">Connected socket</param>
+		/// <returns>string</returns>
+		public static string receiveStringWithSocket(Socket connectionSocket) {
+			//Get size of incoming object
+			byte[] sizeOfIncomingBuffer = new byte[sizeof(int)];
+			connectionSocket.Blocking = true;
+			connectionSocket.Receive(sizeOfIncomingBuffer, 0, sizeof(int), SocketFlags.None);
+			//extract int
+			int sizeOfIncoming = BitConverter.ToInt32(sizeOfIncomingBuffer);
+			//receive main object
+			byte[] incomingObjectBuffer = new byte[sizeOfIncoming];
+			connectionSocket.Receive(incomingObjectBuffer, 0, sizeOfIncoming, SocketFlags.None);
+			//get string from object
+			string receivedObj = Encoding.UTF8.GetString(incomingObjectBuffer);
+			//Trim null bytes
+			receivedObj = receivedObj.TrimEnd('\0');
+			return receivedObj;
+		}
+
+		/// <summary>
+		/// Send a string with socket
+		/// </summary>
+		/// <param name="str">String to send</param>
+		/// <param name="connectionSocket">Connected socket</param>
+		public static void sendStringWithSocket(string str, Socket connectionSocket) {
+			//Send name
+			string encodingTarget = str;
+			byte[] stringBuffer = Encoding.UTF8.GetBytes(encodingTarget);
+			byte[] sizeBuffer = BitConverter.GetBytes(stringBuffer.Length);
+			connectionSocket.Blocking = true;
+			connectionSocket.Send(sizeBuffer, sizeof(int), SocketFlags.None);
+
+			//Send message string to client
+			connectionSocket.Send(stringBuffer, stringBuffer.Length, SocketFlags.None);
+		}
+
+		#region Json
+
+		/// <summary>
+		/// Serialize object to json and send it
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="obj"></param>
+		/// <param name="socket"></param>
+		public static void sendJsonObjectWithSocket<T>(T obj, Socket socket) {
+			string json = JsonSerializer.Serialize(obj);
+			sendStringWithSocket(json, socket);
+		}
+
+		/// <summary>
+		/// Receive string and deserialize to object from json
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="socket"></param>
+		/// <returns></returns>
+		public static T receiveJsonObjectWithSocket<T>(Socket socket) {
+			string json = receiveStringWithSocket(socket);
+			T receivedObj = JsonSerializer.Deserialize<T>(json);
+			return receivedObj;
+		}
+
+		#endregion Json
+
+		#region Serialize Objects to binary
+
+		/// <summary>
 		/// Send an object with the socket
 		/// </summary>
 		/// <param name="input">Object inheriting ISerializableObject</param>
@@ -53,70 +121,6 @@ namespace Networking_Library {
 			return bytes;
 		}
 
-		/// <summary>
-		/// Receive an string with the given socket
-		/// </summary>
-		/// <param name="connectionSocket">Connected socket</param>
-		/// <returns>string</returns>
-		public static string receiveStringWithSocket(Socket connectionSocket) {
-			//Get size of incoming object
-			byte[] sizeOfIncomingBuffer = new byte[sizeof(int)];
-			connectionSocket.Blocking = true;
-			connectionSocket.Receive(sizeOfIncomingBuffer, 0, sizeof(int), SocketFlags.None);
-			//extract int
-			int sizeOfIncoming = BitConverter.ToInt32(sizeOfIncomingBuffer);
-			//receive main object
-			byte[] incomingObjectBuffer = new byte[sizeOfIncoming];
-			connectionSocket.Receive(incomingObjectBuffer, 0, sizeOfIncoming, SocketFlags.None);
-			//get string from object
-			string receivedObj = Encoding.ASCII.GetString(incomingObjectBuffer);
-			//Trim null bytes
-			receivedObj = receivedObj.TrimEnd('\0');
-			return receivedObj;
-		}
-
-		/// <summary>
-		/// Send a string with socket
-		/// </summary>
-		/// <param name="str">String to send</param>
-		/// <param name="connectionSocket">Connected socket</param>
-		public static void sendStringWithSocket(string str, Socket connectionSocket) {
-			//Send name
-			string encodingTarget = str;
-			byte[] stringBuffer = Encoding.ASCII.GetBytes(encodingTarget);
-			byte[] sizeBuffer = BitConverter.GetBytes(stringBuffer.Length);
-			connectionSocket.Blocking = true;
-			connectionSocket.Send(sizeBuffer, sizeof(int), SocketFlags.None);
-
-			//Send message string to client
-			connectionSocket.Send(stringBuffer, stringBuffer.Length, SocketFlags.None);
-		}
-
-		#region Json
-
-		/// <summary>
-		/// Serialize object to json and send it
-		/// </summary>
-		/// <typeparam name="T"></typeparam>
-		/// <param name="obj"></param>
-		/// <param name="socket"></param>
-		public static void sendJsonObjectWithSocket<T>(T obj, Socket socket) {
-			string json = JsonSerializer.Serialize(obj);
-			sendStringWithSocket(json, socket);
-		}
-
-		/// <summary>
-		/// Receive string and deserialize to object from json
-		/// </summary>
-		/// <typeparam name="T"></typeparam>
-		/// <param name="socket"></param>
-		/// <returns></returns>
-		public static T receiveJsonObjectWithSocket<T>(Socket socket) {
-			string json = receiveStringWithSocket(socket);
-			T receivedObj = JsonSerializer.Deserialize<T>(json);
-			return receivedObj;
-		}
-
-		#endregion Json
+		#endregion Serialize Objects to binary
 	}
 }
