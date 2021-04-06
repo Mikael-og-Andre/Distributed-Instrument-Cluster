e409dbf7cbe8849cc80e7d73eb8de986073ff4c1
Mikael Nilssen <Nilssen-m@hotmail.com>
2021-04-06 22:48:35 +0200

    Restructured frontend Code for Device component, KeyaboardMouse, Video

---
+++ Distributed Instrument Cluster/Blazor Instrument Cluster/Client/Code/DeviceComponent.razor.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Blazor_Instrument_Cluster.Shared;
+using Microsoft.AspNetCore.Components;
+
+namespace Blazor_Instrument_Cluster.Client.Code {
+	/// <summary>
+	/// Get DeviceModel passed in an use it to display info
+	/// <author>Mikael Nilssen</author>
+	/// </summary>
+	public class DeviceComponent : ComponentBase{
+		
+		[Inject]
+		private NavigationManager navigationManager { get; set; }
+
+		[Parameter]
+		public DeviceModel deviceInfo { get; set; }
+
+		private string basePath = "VideoAndControl";
+
+		protected void navigateToDevicePage() {
+
+			string crestronSubname = "crestronControl";
+			string videoSubname = "videoStream";
+
+			foreach (var subName in deviceInfo.subNames) {
+
+				if (subName.Contains("crestron")) {
+					crestronSubname = subName;
+				}
+				if (subName.Contains("video")) {
+					videoSubname = subName;
+				}
+
+			}
+
+			string fullPath = basePath + "/" + deviceInfo.name + "/" + deviceInfo.location + "/" + deviceInfo.type + "/" + crestronSubname + "/" + videoSubname;
+
+			navigationManager.NavigateTo(fullPath);
+		}
+	}
+}
