3391300ad10e751427399aa4208415b5c979cc0e
Mikael Nilssen <Nilssen-m@hotmail.com>
2021-04-11 18:49:15 +0200

    Updated String sending to use New byte sending

--- Distributed Instrument Cluster/NetworkingLibrary/NetworkingOperations.cs
+++ Distributed Instrument Cluster/NetworkingLibrary/NetworkingOperations.cs
@@ -16,18 +16,13 @@ namespace Networking_Library {
 		/// <param name="connectionSocket">Connected socket</param>
 		/// <returns>string</returns>
 		public static string receiveStringWithSocket(Socket connectionSocket) {
-			//Get size of incoming object
-			byte[] sizeOfIncomingBuffer = new byte[sizeof(int)];
-			connectionSocket.Blocking = true;
-			connectionSocket.Receive(sizeOfIncomingBuffer, 0, sizeof(int), SocketFlags.None);
-			//extract int
-			int sizeOfIncoming = BitConverter.ToInt32(sizeOfIncomingBuffer);
-			//receive main object
-			byte[] incomingObjectBuffer = new byte[sizeOfIncoming];
-			connectionSocket.Receive(incomingObjectBuffer, 0, sizeOfIncoming, SocketFlags.None);
-			//get string from object
-			string receivedObj = Encoding.UTF32.GetString(incomingObjectBuffer);
-			return receivedObj;
+			
+			NetworkStream networkStream = new NetworkStream(connectionSocket, true);
+			//receive bytes with socket stream
+			byte[] incomingBytes=receiveBytes(networkStream);
+			//get string from bytes
+			string receivedString = Encoding.UTF32.GetString(incomingBytes);
+			return receivedString;
 		}
 
 		/// <summary>
@@ -35,19 +30,15 @@ namespace Networking_Library {
 		/// </summary>
 		/// <param name="str">String to send</param>
 		/// <param name="connectionSocket">Connected socket</param>
-		public static void sendStringWithSocket(string str, Socket connectionSocket) {
+		public static void sendStringWithSocket(string inputString, Socket connectionSocket) {
 			//Send name
-			string encodingTarget = str;
-			byte[] stringBuffer = Encoding.UTF32.GetBytes(encodingTarget);
-			byte[] sizeBuffer = BitConverter.GetBytes(stringBuffer.Length);
-			connectionSocket.Blocking = true;
-			connectionSocket.Send(sizeBuffer, sizeof(int), SocketFlags.None);
-
-			//Send message string to client
-			connectionSocket.Send(stringBuffer, stringBuffer.Length, SocketFlags.None);
+			byte[] stringBuffer = Encoding.UTF32.GetBytes(inputString);
+			//send data with stream
+			NetworkStream networkStream = new NetworkStream(connectionSocket,true);
+			sendBytes(networkStream,stringBuffer);
 		}
 
-		#region Json
+#region Json
 
 		/// <summary>
 		/// Serialize object to json and send it
