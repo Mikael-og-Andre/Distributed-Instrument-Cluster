f8190623cab889e8cc264fc830178c6753bb17a6
Mikael Nilssen <Nilssen-m@hotmail.com>
2021-04-22 18:00:27 +0200

    Receiving Client Tester Updated

--- Distributed Instrument Cluster/ReceivingClientTester/Program.cs
+++ Distributed Instrument Cluster/ReceivingClientTester/Program.cs
@@ -17,14 +17,9 @@ namespace ReceivingClientTester {
 
 	        ReceivingClient receivingClient = new ReceivingClient("127.0.0.1", 6981,
 		        new ClientInformation("Radar1", "device location", "device type","crestronControl"), new AccessToken("access"),new CancellationToken(false));
-
-	        Task.Run(() => {
-				receivingClient.run();
-	        });
-
 	        while (true) {
 		        if (receivingClient.receiveBytes(out byte[] output)) {
-			        CrestronCommand obj = JsonSerializer.Deserialize<CrestronCommand>(Encoding.UTF8.GetString(output).TrimStart('\0').TrimEnd('\0'));
+			        CrestronCommand obj = JsonSerializer.Deserialize<CrestronCommand>(Encoding.UTF8.GetString(output).TrimEnd('\0'));
 					Console.WriteLine("Received object text: {0}",obj.msg);
 		        }
 				Thread.Sleep(10);
