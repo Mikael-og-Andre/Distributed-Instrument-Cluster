afac0b997085be46aa7fc6796ac16f17349d2f89
Mikael Nilssen <Nilssen-m@hotmail.com>
2021-05-06 20:38:08 +0200

    Removed ProgramAsync Code and added throw not yet implemented

--- Distributed_Instrument_Cluster/CrestronReceiver/ProgramAsync.cs
+++ Distributed_Instrument_Cluster/CrestronReceiver/ProgramAsync.cs
@@ -13,22 +13,7 @@ namespace ReceivingClientTester {
 	public class ProgramAsync {
 		static void Main(string[] args) {
 
-			Thread.Sleep(10000);
-			Console.WriteLine("Starting client...");
-
-			AccessToken accessToken = new AccessToken("auth");
-			DuplexClientAsync client = new DuplexClientAsync("192.168.50.62", 6981,accessToken);
-
-			Console.WriteLine("Setting up");
-			client.setup().Wait();
-			Console.WriteLine("Setup Complete");
-
-			while (true) {
-				byte[] bytes= client.receiveBytesAsync().Result;
-				string rec = Encoding.UTF32.GetString(bytes);
-				Console.WriteLine("Received: {0}",rec);
-			}
-
+			throw new NotImplementedException();
 		}
 	}
 }
